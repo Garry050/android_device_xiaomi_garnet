9df98433f3d5afa94a61e8847d9f62d2967ef23f
Garry050 <garry.windows8@gmail.com>
2024-06-26 08:12:53 +0900

    garnet: repo: update vendorsetup.sh to own repository

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,20 +1,20 @@
 echo 'Starting to clone stuffs needed to build for garnet'
 
-# Vendor
-# echo 'Cloning vendor tree'
-# rm -rf vendor/xiaomi/garnet && git clone https://github.com/JYRRC/vendor_xiaomi_garnet.git -b 14.0 vendor/xiaomi/garnet
+#Vendor
+echo 'Cloning vendor tree'
+git clone https://github.com/Garry050/vendor_xiaomi_garnet.git -b 14.0 vendor/xiaomi/garnet
 
 # Kernel
 echo 'Cloning kernel tree'
-git clone https://github.com/JYRRC/device_xiaomi_garnet-kernel.git -b ksu device/xiaomi/garnet-kernel
+git clone https://github.com/Garry050/android_device_xiaomi_garnet-prebuilt.git -b lineage-21 device/xiaomi/garnet-kernel
 
 # Hardware Xiaomi
-# echo 'Cloning hardware oplus'
-# rm -rf hardware/xiaomi && git clone https://github.com/JYRRC/hardware_xiaomi.git -b 14.0 hardware/xiaomi
+echo 'Cloning hardware xiaomi'
+git clone https://github.com/Garry050/android_hardware_xiaomi.git -b 14.0 hardware/xiaomi
 
 # MiuiCamera
-# echo 'Cloning common tree'
-# git clone https://codeberg.org/JYRRC/vendor_xiaomi_garnet-miuicamera.git -b 14-leica vendor/xiaomi/garnet-miuicamera
+echo 'Cloning MiuiCamera tree'
+git clone https://git.mainlining.org/adarshgrewal/proprietary_vendor_xiaomi_garnet-miuicamera.git -b lineage-21 vendor/xiaomi/garnet-miuicamera
 
 # PixelParts
 #echo 'Cloning vendor tree'
